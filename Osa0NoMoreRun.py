9b55608e037e75bd6a2638544ef5c61d4c114bd7
Balde641 <62099893+Balde641@users.noreply.github.com>
2020-10-05 20:00:42 +0300

    New1

---
+++ Osa0NoMoreRun.py
@@ -0,0 +1,31 @@
+# Using Databases SQLite: Have a look at 16-->17-->18-->19
+
+from tkinter import *
+from PIL import ImageTk,Image
+import sqlite3
+
+root = Tk()
+root.title('Learn To code at Codemy.com')
+root.iconbitmap('/Users/baldez/Documents/GitHub/TkInter/images/Balde4.png')
+root.geometry("400x400")
+
+# Create a database or connect to one
+conn = sqlite3.connect('tasklist.db')
+
+# Create a cursor
+c = conn.cursor()
+
+# Create table
+c.execute("""CREATE TABLE tasks (
+    task text
+)
+""")
+
+# Commit Changes
+conn.commit()
+
+# Close connection
+conn.close()
+
+
+root.mainloop()
