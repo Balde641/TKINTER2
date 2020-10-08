be298f3da4374769a328099c257b59ef792e7195
Balde641 <62099893+Balde641@users.noreply.github.com>
2020-10-08 11:12:28 +0300

    08.10.20

--- Osa1.py
+++ Osa1.py
@@ -80,7 +80,7 @@ def edit():
         
     # Create a Save Button To Save Edited Record
     edit_btn = Button(editor, text="Talenna Tietue", command=update, fg="green")
-    edit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=140)
+    edit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=10)
 
 
 # Create Function to Delete A Record
@@ -158,19 +158,19 @@ task_label_label.grid(row=0, column=0, pady=(10, 0))
 
 # Create Submit Button
 submit_btn = Button(root, text="Lisää Tietue Tietokantaan", command=submit, fg="green")
-submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=10)
 
 # Create a Query Button
 query_btn = Button(root, text="Näytä Tietueet", command=query, fg="brown")
-query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=10)
 
 # Create a Delete Button
 delete_btn = Button(root, text="Poista Tietueet", command=delete, fg="red")
-delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=136)
+delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=10)
 
 # Create an Update Button
 edit_btn = Button(root, text="Muokkaa Tietueita", command=edit, fg="blue")
-edit_btn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=130)
+edit_btn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=10)
 
 # Commit Changes
 conn.commit()
@@ -186,16 +186,82 @@ l = Label(root, text = "Käyttäjätunnus:")
 l.grid(row = 13, column = 0)
 e = Entry(root, width = 30, bg="orange", fg="black", borderwidth=5)
 e.grid(row = 13, column = 1)
-e.insert(0, "Nimi: ")
+#e.insert(0, "Nimi: ")
 # rivi 1 -------------------------------------------
 l2 = Label(root, text = "Salasana:")
 l2.grid(row = 14, column = 0)
 e2 = Entry(root, width = 30, show = '*', bg="orange", fg="black", borderwidth=5)
 e2.grid(row = 14, column = 1)
-e2.insert(0, "Salasana: ")
+#e2.insert(0, "Salasana: ")
 # rivi 2 -------------------------------------------
 b = Button(root, text = "KIRJAUDU SISÄÄN", fg="green")
 b.grid(row = 15, column = 1)
 
 # pitää ikkunan näkyvissä kunnes tuhoamme sen
+root.mainloop() 
+
+#==============================
+# Creating a login system with pictures
+
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk
+
+
+class Login_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Kirjautumisjärjestelmä")
+        self.root.geometry("700x500")
+
+        #===All Images
+        self.bg_icon=ImageTk.PhotoImage(file="img3/log1.jpg")
+        self.user_icon=ImageTk.PhotoImage(file="img3/userB.png")
+        self.pass_icon=ImageTk.PhotoImage(file="img3/pas.jpg")
+        self.pass2_icon=ImageTk.PhotoImage(file="img3/log.png")
+
+
+        #====Variables==
+        
+        self.uname=StringVar()
+        self.pass_=StringVar()
+        
+        bg_lbl=Label(self.root,image=self.bg_icon)
+        bg_lbl.pack()
+
+        title=Label(self.root,text="Kirjautumisjärjestelmä",font=("times new roman",40,"bold"),bg="green",fg="yellow",bd=10,relief=GROOVE)
+        title.place(x=0,y=0,relwidth=1)
+
+        Login_Frame=Frame(self.root,bg="white")
+        Login_Frame.place(x=200,y=70)
+
+        #logolbl
+        lbl_pass2=Label(Login_Frame,image=self.pass2_icon,bd=0)
+        lbl_pass2.grid(row=0,columnspan=2, pady=20)
+
+        lbluser=Label(Login_Frame,text="Käyttäjätunnus",image=self.user_icon,compound=LEFT,font=("times new roman",15,"bold"),bg="white")
+        lbluser.grid(row=1,column=0,padx=0,pady=0)
+        textuser=Entry(Login_Frame,bd=5,textvariable=self.uname,relief=GROOVE,font=("",15))
+        textuser.grid(row=1,column=1, padx=20)
+
+        lbl_pass=Label(Login_Frame,text="Salasana",image=self.pass_icon,compound=LEFT,font=("times new roman",15,"bold"),bg="white")
+        lbl_pass.grid(row=2,column=0,padx=20,pady=10)
+        text_pass=Entry(Login_Frame,bd=5,relief=GROOVE,textvariable=self.pass_,font=("",15))
+        text_pass.grid(row=2,column=1, padx=20)
+        
+        btn_log=Button(Login_Frame,text="Kirjaudu sisään",width=15,command=self.login,font=("times now roman",14,"bold"),bg="yellow",fg="red")
+        btn_log.grid(row=3,column=1,pady=10)
+
+
+    def login(self):
+        if self.uname.get()=="" or self.pass_.get()=="":
+             messagebox.showinfo("Error", "Kaikki kentät ovat pakollisia!!")
+        elif self.uname.get()=="Balde" and self.pass_.get()=="123":
+             messagebox.showinfo("Onnistunut",f"Tervetuloa! {self.uname.get()}" "\n" 
+             "Tarkista tehtävät täältä: Asentaminen, päivittäminen, simulointi, ylläpito.") 
+        else:
+            messagebox.showerror("Error","Väärä käyttäjänimi tai salasana")         
+
+root=Tk()
+object=Login_System(root)
 root.mainloop() 
